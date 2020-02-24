89cb3f433a369c3ca5c6bc400184c190a5f3602f
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-02-24 17:18:24 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricmarioProvjeraZnanjaZadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisi rijec: ");
+            string text = Console.ReadLine();
+
+            for (int n = 1; n <= text.Length; n++)
+            {
+                for (int j = text.Length - n; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(text.Substring(text.Length - n));
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+
+        
+        }
+    }
+}
